a57fb851e1d61ab7ae4839fe8e3cda9512f96433
kusi <lawcubegsm@gmail.com>
2022-08-19 10:21:38 +0000

    Fix css/hml error

--- src/modules/createElements.js
+++ src/modules/createElements.js
@@ -17,21 +17,6 @@ const createMyElement = (el, cl = '', ids = '') =>{
 
 const addListToPage = (ul,ob) => {
  ob.forEach((obj) => {
- //   ul.innerHTML += `
- //  <li class="list-item">
- //  <div>
-
- //  <input type="checkbox" name="checkbox" id="id-${obj.index}" value=${obj.complete}>
- //  <input type="text" id="edit-input-${obj.index}", class = "edit-input">
- //  <label for="id-${obj.index}" id="label-${obj.index}">${obj.description}</label>
- //  </div>
- //  <div>
- //  <span class="collapse" id='col-${obj.index}'><i class="fa-solid fa-ellipsis-vertical"></i></span>
- //  <span class="delete-icon"  id='del-${obj.index}'><i class="fa-solid fa-trash-can"></i></span>
- //  </div>
- // </li>
- //  `;
-
   const div = createMyElement('div');
   const checkbox = createMyElement('input','', `id-${obj.index}`)
   checkbox.setAttribute('type', 'checkbox')
