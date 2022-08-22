322dc0e0f861bb80b16dddb021ce6eaa1bbae040
kusi <lawcubegsm@gmail.com>
2022-08-22 16:28:11 +0000

    Update yaml run-on: fron 18.0 to ubuntu-20.04 to avoid depreciation error

--- src/modules/toggle.js
+++
@@ -1,6 +0,0 @@
-const toggle = (objIndex, extra = {}) => {
-  document.getElementById(`${extra.selfIdExtension}-${objIndex}`).style.display = extra.selfDisplay;
-  document.getElementById(`${extra.otherIdExtension}-${objIndex}`).style.display = extra.otherDisplay;
-};
-
-export default toggle;
