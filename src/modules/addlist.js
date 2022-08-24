0feb56322e272efa58b53bd6a8808fffe1aa8382
kusi <lawcubegsm@gmail.com>
2022-08-24 21:37:58 +0000

    Add add-to-list test

--- src/modules/addlist.js
+++ src/modules/addlist.js
@@ -1,7 +1,12 @@
-const addToList = (obj, text) => obj.concat({
+const addToList = (obj, text) => {
+if (text.trim().length !== 0){
+ return obj.concat({
   index: obj.length,
   description: text,
   completed: false,
 });
-
+}else{
+ return obj
+}
+}
 export default addToList;
