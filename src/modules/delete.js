ea7e6c42f04eaed27e7aebbe8690c0deb8022841
kusi <lawcubegsm@gmail.com>
2022-08-24 22:15:05 +0000

    Fix delete test bug

--- src/modules/delete.js
+++ src/modules/delete.js
@@ -1,5 +1,5 @@
 const deleteItem = (obj, index) => {
-if(Number.isInteger(index)){
+if(Number.isInteger(parseInt(index, 10))){
  return obj.filter((list) => list.index !== parseInt(index, 10));
 }{
  return obj
