7e4803c4aa7cb7d539faee88317a64cb515fa47f
Tommymarvel <ibironketomiwa4@gmail.com>
2022-08-25 10:50:39 +0100

    Add delete allcompleted test

--- src/modules/delete.test.js
+++ src/modules/delete.test.js
@@ -60,3 +60,51 @@ describe('deleteItem', () => {
  });
 
 });
+
+describe("deleteComplete", () => {
+  test("Delete all data mark with completed as true", () => {
+    //Arrange
+    const array = [
+      { index: 0, description: "I will join standup team", completed: 'false' },
+      { index: 1, description: "Meeting at 1pm", completed: 'true' },
+    ];
+    //Act
+    const result = deleteComplete(array);
+    //Assert
+    expect(result).toStrictEqual([
+      {
+        index: 0,
+        description: "I will join standup team",
+        completed: 'false',
+      },
+    ]);
+  });
+  test("Delete all data mark with completed as true, all data should deleted", () => {
+    //Arrange
+    const array = [
+      { index: 0, description: "I will join standup team", completed: 'true' },
+      { index: 1, description: "Meeting at 1pm", completed: 'true'},
+      { index: 2, description: "Meeting at 6pm", completed: 'true' },
+    ];
+    //Act
+    const result = deleteComplete(array);
+    //Assert
+    expect(result).toStrictEqual([]);
+  });
+  test("Delete data using floating point number instead of integer, all data should deleted", () => {
+    //Arrange
+    const array = [
+      { index: 0, description: "I will join standup team", completed: 'false' },
+      { index: 1, description: "Meeting at 1pm", completed: 'false' },
+      { index: 1, description: "Meeting at 6pm", completed: 'false' },
+    ];
+    //Act
+    const result = deleteComplete(array);
+    //Assert
+    expect(result).toStrictEqual([
+      { index: 0, description: "I will join standup team", completed: 'false' },
+      { index: 1, description: "Meeting at 1pm", completed: 'false'},
+      { index: 1, description: "Meeting at 6pm", completed: 'false' },
+    ]);
+  });
+});
